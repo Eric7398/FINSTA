07f024edc37c9043b3d0e3486c81f4344c865ebf
Eric Wang <ericwang7398@gmail.com>
2021-11-23 17:45:09 -0800

    Completed Sign in

--- components/Header.js
+++ components/Header.js
@@ -8,21 +8,28 @@ import {
     MenuIcon,
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { useRouter } from "next/router";
 
 function Header() {
+    const { data: session } = useSession();
+    const router = useRouter();
+
+
+
     return (
         <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
             <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
 
                 {/* Left */}
-                <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
+                <div onClick={() => router.push('/')} className='relative hidden lg:inline-grid w-24 cursor-pointer'>
                     <Image src='https://links.papareact.com/ocw'
                         layout='fill'
                         objectFit='contain'
                     />
                 </div>
 
-                <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
+                <div onClick={() => router.push('/')} className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
                     <Image src='https://links.papareact.com/jjm'
                         layout='fill'
                         objectFit='contain'
@@ -44,18 +51,26 @@ function Header() {
 
                 {/* Right */}
                 <div className='flex items-center justify-end space-x-4'>
-                    <HomeIcon className='navBtn' />
+                    <HomeIcon onClick={() => router.push('/')} className='navBtn' />
                     <MenuIcon className='h-6 md:hidden cursor-pointer' />
 
-                    <div className='relative navBtn'>
-                        <PaperAirplaneIcon className='navBtn' />
-                        <div className='absolute -top-1 -right-1.5 text-sx w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
-                    </div>
-                    <PlusCircleIcon className='navBtn' />
-                    <UserGroupIcon className='navBtn' />
-                    <HeartIcon className='navBtn' />
+                    {session ? (
+                        <>
+                            <div className='relative navBtn'>
+                                <PaperAirplaneIcon className='navBtn' />
+                                <div className='absolute -top-1 -right-1.5 text-sx w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
+                            </div>
+                            <PlusCircleIcon className='navBtn' />
+                            <UserGroupIcon className='navBtn' />
+                            <HeartIcon className='navBtn' />
 
-                    <img src='https://avatars.githubusercontent.com/u/72224088?v=4' alt='profile picture' className='h-10 rounded-full cursor-pointer'></img>
+                            <img onClick={signOut} src={session.user.image} alt='profile picture' className='h-10 w-10 rounded-full cursor-pointer'></img>
+                        </>
+                    ) : (
+                        <button onClick={signIn}>Sign In</button>
+
+
+                    )}
                 </div>
 
             </div>
---
+++ firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC6LG9Xk-kxJT6xC9nlkVYs31BnN1_22_I",
+    authDomain: "finsta-9b9a5.firebaseapp.com",
+    projectId: "finsta-9b9a5",
+    storageBucket: "finsta-9b9a5.appspot.com",
+    messagingSenderId: "768971286712",
+    appId: "1:768971286712:web:2a5de5d0b1f7cf39db2c4d",
+    measurementId: "G-8TZLRVWJ2H"
+};
+
+// Initialize Firebase
+const app = !getApp().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
+const analytics = getAnalytics(app);
